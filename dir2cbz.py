e9d9885d697845460b1adab4b0a6037e5831ca70
Stephane Duguay <s@binarez.com>
2018-02-25 15:50:01 -0500

    Initial commit

---
+++ dir2cbz.py
@@ -0,0 +1,23 @@
+#! python3
+# Scans all subdirectories and creates CBZ from files in them
+# Subdirectories must contain valid image files
+
+# @author Stephane Duguay // binarez
+
+import os
+import zipfile
+
+def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
+
+def zipdir(path, ziph):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
+subdirs = get_immediate_subdirectories(os.getcwd())
+for subdir in subdirs:
+	zipf = zipfile.ZipFile( subdir + '.cbz', 'w', zipfile.ZIP_DEFLATED)
+	zipdir(subdir, zipf)
+	zipf.close()
